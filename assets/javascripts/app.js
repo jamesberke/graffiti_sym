e17cdaa0a7297530c04e98c70a6c2ddf4081361b
James Berke <james.berke@gmail.com>
2020-03-29 14:18:26 -0700

    began work on canvas

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,4 +1,29 @@
 const canvas = documant.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
+let isDrwaing = false;
+
+canvas.addEventListener('mousedown', start);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', stop);
+canvas.addEventListener('mouseout', stop);
+
+function start(e) {
+    isDrwaing = true; 
+};
+
+function draw({clientX, clientY}) {
+    if (!isDrwaing) return;
+    ctx.lineWidth = 3;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#'
+}
+
+
+window.addEventListener('resize', resizeCanvas);
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+};
+
