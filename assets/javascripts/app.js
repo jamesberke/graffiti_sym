a069f2d1e678f0f58078d634ac3435e5c3645246
James Berke <james.berke@gmail.com>
2020-03-29 17:08:01 -0700

    added color selector and clear canvas

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -2,11 +2,16 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let isDrwaing = false;
+const clearButton = document.getElementById('toolbar-clear-canvas');
+const colorPicker = document.querySelector('.toolbar-color-selector');
 
 canvas.addEventListener('mousedown', start);
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', stop);
 canvas.addEventListener('mouseout', stop);
+
+clearButton.addEventListener('click', clearCanvas);
+colorPicker.addEventListener('change', chooseColor);
 
 function start(e) {
     isDrwaing = true; 
@@ -17,7 +22,6 @@ function draw({clientX, clientY}) {
     if (!isDrwaing) return;
     ctx.lineWidth = 3;
     ctx.lineCap = 'round';
-    ctx.strokeStyle = '#AC2542'
 
     ctx.lineTo(clientX, clientY);
     ctx.stroke();
@@ -34,6 +38,9 @@ function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);   
 }
 
+function chooseColor(e) {
+   ctx.strokeStyle = e.target.value;
+}
 
 window.addEventListener('resize', resizeCanvas);
 
